49a1e377f36e1112e2d9e28ea1d58ddd0183700c
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-04-05 14:34:43 -0600

    added NPM support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 set -e
-ROJO_CONFIG="dev.project.json"
-DARKLUA_CONFIG=".darklua.json"
-SOURCEMAP="darklua-sourcemap.json"
-MODEL_ROJO_CONFIG="model.project.json"
+ROJO_CONFIG=$1
+# if ROJO_CONFIG = "" then error
+if [ -z "$ROJO_CONFIG" ]; then
+	echo "ROJO_CONFIG is not set"
+	exit 1
+fi
+
+DARKLUA_CONFIG=.darklua.json
+SOURCEMAP=sourcemap.json
 LSP_SETTINGS=".luau-analyze.json"
+
 # get if any of the arguments were "--serve"
 is_serve=false
 build_dir="build"
-is_wally=true
-
-# if [ ! -d node_modules ]; then
-#     sh scripts/npm-install.sh
-# fi
-
 for arg in "$@"
 do
 	if [ "$arg" = "--serve" ]; then
 		is_serve=true
 		build_dir="serve"
 	fi
-
-	# if [ "$arg" = "--wally" ]; then
-	# 	echo "wally project detected"
-	# 	is_wally=true
-	# fi
 done
 
 # create build directory
@@ -36,44 +31,28 @@ mkdir -p $build_dir
 echo "copying contents to $build_dir"
 
 cp -r "$ROJO_CONFIG" "$build_dir/$ROJO_CONFIG"
-cp -r "$MODEL_ROJO_CONFIG" "$build_dir/$MODEL_ROJO_CONFIG"
-cp -r "$DARKLUA_CONFIG" "$build_dir/$DARKLUA_CONFIG"
-cp -r "default.project.json" "$build_dir/default.project.json"
-
-if [ "$is_wally" = true ]; then
-	cp -r "wally.toml" "$build_dir/wally.toml"
-	cp -r "aftman.toml" "$build_dir/aftman.toml"
-fi
-
 cp -r "src" "$build_dir/src"
-cp -rL "Packages" "$build_dir/Packages"
-cp -rL "scripts" "$build_dir/scripts"
-
 cp -r "types" "$build_dir/types"
 cp -r "lints" "$build_dir/lints"
-
 cp -r "selene.toml" "$build_dir/selene.toml"
 cp -r "stylua.toml" "$build_dir/stylua.toml"
 cp -r "$LSP_SETTINGS" "$build_dir/$LSP_SETTINGS"
 
-echo "build sourcemap"
-rojo sourcemap "$MODEL_ROJO_CONFIG" -o "$SOURCEMAP"
+# build sourcemaps
+echo "building sourcemaps"
+rojo sourcemap "$ROJO_CONFIG" -o "$SOURCEMAP"
+rojo sourcemap "$build_dir/$ROJO_CONFIG" -o "$build_dir/$SOURCEMAP"
 
 # process files
 echo "running stylua"
 stylua "$build_dir/src"
 
 # run darklua
+echo "running darklua"
 if [ "$is_serve" = true ]; then
-	echo "running serve darklua"
-	rojo sourcemap --watch "$ROJO_CONFIG" -o "$SOURCEMAP" &
-	darklua process "src" "$build_dir/src" --config "$DARKLUA_CONFIG" -w & 
-	# darklua process "node_modules" "$build_dir/node_modules" --config "$DARKLUA_CONFIG" -w & 
+	darklua process "src" "$build_dir/src" --config "$DARKLUA_CONFIG" -w &
 else
-	echo "running build darklua"
-	rojo sourcemap "$build_dir/$ROJO_CONFIG" -o "$build_dir/$SOURCEMAP"
-	darklua process "src" "$build_dir/src" --config "$DARKLUA_CONFIG" --verbose
-	# darklua process "node_modules" "$build_dir/node_modules" --config "$DARKLUA_CONFIG" --verbose
+	darklua process "src" "$build_dir/src" --config "$DARKLUA_CONFIG"
 fi
 
 # final compile
@@ -84,8 +63,4 @@ else
 	echo "build rbxl"
 	cd "$build_dir"
 	rojo build "$ROJO_CONFIG" -o "Package.rbxl"
-	if [ "$is_wally" = true ]; then
-		rm -rf "$build_dir/$MODEL_ROJO_CONFIG"
-		rm -rf "$build_dir/$ROJO_CONFIG"
-	fi
 fi
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
+ROJO_CONFIG=$1
+# if ROJO_CONFIG = "" then error
+if [ -z "$ROJO_CONFIG" ]; then
+	echo "ROJO_CONFIG is not set"
+	exit 1
+fi
 sh scripts/download-types.sh
-sh scripts/build.sh "$1"
+sh scripts/build.sh "$ROJO_CONFIG"
 cd "build"
 luau-lsp analyze \
-	--sourcemap="darklua-sourcemap.json" \
+	--sourcemap="sourcemap.json" \
 	--ignore="**/Packages/**" \
 	--ignore="Packages/**" \
 	--ignore="*.spec.luau" \
